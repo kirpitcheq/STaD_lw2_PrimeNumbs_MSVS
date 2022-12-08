6ef68cd91093ef5746ac0c9180474f4217c2704c
ElektoBass <ElektoBass@ya.ru>
2022-12-08 16:35:05 +0300

    Correct miller, add Sieve

--- PrimeNumbs/PrimeNumbs.cpp
+++ PrimeNumbs/PrimeNumbs.cpp
@@ -20,48 +20,62 @@ bool is_prime_miller_rabin(unsigned long long int num, int rounds)
 {
 	if (num == 2 || num == 3)
 		return true;
-	if (num % 2 != 0 || num < 2)
+	if (num % 2 == 0 || num < 2)
 		return false;
 	unsigned long long int d = num - 1;
 	int s = 0;
-	unsigned long long int t = d;
-	while (d % 2 != 0)
+	while (d % 2 != 0) // n-1 = 2^s * d get
 	{
-		t /= 2;
+		d /= 2;
 		s++;
 	}
 	srand(time(NULL));
-	for (size_t i = 0; i < rounds; i++)
+	for (auto i = 0; i < rounds; i++)
 	{
-		unsigned long long int rand_num = rand() % num;
-		unsigned long long int x = pow((int)rand_num, (int)t) % num;
-		if (x == 1 || x == -1)
-			continue;
-		for (size_t i = 0; i < s; i++)
+		unsigned long long int rand_num = 0;
+		while (rand_num < 2 || rand_num > num - 2)
+			rand_num = rand() % (num - 2);
+		unsigned long long int a_pow_d_mod_n = pow(rand_num, d);
+		a_pow_d_mod_n %= num;
+		for (auto i = 0; i < s; i++)
 		{
-			x = (x * x) % num;
-			if (x == 1 && x != 1 && x != num-1)
+			auto x = (a_pow_d_mod_n * a_pow_d_mod_n) % num;
+			if (x == 1 && x != 1 && x != num - 1)
 				return false;
-			if (x == num - 1)
-				break;
+			if (a_pow_d_mod_n != 1)
+				return false;
 		}
+		return true;
 	}
 
 	return true; //Может возвращать вероятность?
 }
 
-bool is_prime_oprim(unsigned int num)
+bool is_prime_sieve_eratosthenes(unsigned int num)
 {
+	bool* crossed_numbs = new bool[num + 1] {}; //init with false
+	for (auto i = 2; i <= num; i++)
+	{
+		if (crossed_numbs[i])
+			continue;
+		for (auto j = i + i; j <= num; j += i)
+			crossed_numbs[j] = true;
+	}
+	if (crossed_numbs[num])
+		return false;
 	return true;
-
 }
 
 int main()
 {
-	unsigned int numb;
-	std::cin >> numb;
-	std::cout << is_prime(numb) << std::endl;
-	std::cout << is_prime_miller_rabin(numb, 100) << std::endl;
+	unsigned int numb = 1;
+	while (numb)
+	{
+		std::cin >> numb;
+		std::cout << is_prime(numb) << std::endl;
+		std::cout << is_prime_miller_rabin(numb, 100) << std::endl;
+		std::cout << is_prime_sieve_eratosthenes(numb) << std::endl;
+	}
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
