0c3a76b07b961b082d231920de033f91ffb7b9c0
ElektoBass <ElektoBass@ya.ru>
2022-12-13 18:59:54 +0300

    Added percent out for simple prime function

--- PrimeNumbs/PrimeNumbsKPEq.cpp
+++ PrimeNumbs/PrimeNumbsKPEq.cpp
@@ -2,11 +2,43 @@
 
 bool is_prime(unsigned long long int num)
 {
+	bool print_percent = true;
+	//unsigned short div_numbs = (unsigned short)((double)num / (double) std::numeric_limits<unsigned long long int>::max()) * 100;
+	unsigned short div_numbs = 0;
+	unsigned long long int temp = num;
+	while (temp / 10 != 0)
+	{
+		temp /= 10;
+		div_numbs++;
+	}
+	div_numbs *= div_numbs;
+	div_numbs *= div_numbs;
+
+	if (!div_numbs)
+		print_percent = false;
+	unsigned long long int* percents_left = new unsigned long long int[div_numbs] {};
+	if (print_percent)
+		for (size_t i = 1; i < div_numbs - 1; i++)
+			percents_left[i] = (num) * (double)((double)i / (double)(div_numbs - 1));
+		percents_left[div_numbs - 1] = num;
+	size_t perc_arr_counter = 0;
 	for (size_t i = 2; i < num - 1; i++)
 	{
+		if (print_percent && i > percents_left[perc_arr_counter] && perc_arr_counter < div_numbs)
+		{
+			std::cout << ((double)perc_arr_counter / (double)div_numbs) * 100 << "%-";
+			if (perc_arr_counter != div_numbs - 1)
+				perc_arr_counter++;
+		}
 		if (num % i == 0)
+		{
+			if (print_percent)
+				std::cout << "100%" << std::endl;
 			return false;
+		}
 	}
+	if (print_percent)
+		std::cout << "100%" << std::endl;
 	return true;
 }
 //#define DEBUG_PRINT
@@ -14,11 +46,6 @@ bool is_prime_miller_rabin(US_TP num, size_t rounds)
 {
 	if (num <= 3) return true;
 	if (num == 0) return false;
-	/*
-
-	if (num % 2 == 0 || num < 2 || num % 3 == 0)
-		return false;
-	*/
 	US_TP d = num - 1;
 	US_TP s = 0;
 	while (d % 2 == 0) // n-1 = 2^s * d get
@@ -34,12 +61,12 @@ bool is_prime_miller_rabin(US_TP num, size_t rounds)
 	std::cout << "d = " << d << std::endl;
 	std::cout << "s = " << s << std::endl;
 #endif
-	srand(time(NULL));
+	rd::mt19937 mt;
+	mt.seed(time(NULL));
+	rd::uniform_int_distribution<US_TP> ui(2, num - 2);
 	for (auto i = 0; i < rounds; i++)
 	{
-		US_TP rand_num = 0;
-		while (rand_num < 2 || rand_num > num - 2)
-			rand_num = rand() % (num); //осталось рандом правильный для чисел больше 2^32
+		US_TP rand_num = ui(mt);
 		US_TP a_pow_d_mod_n = power_mod(rand_num, d, num);
 #ifdef DEBUG_PRINT
 		std::cout << "rand_num = " << rand_num << std::endl;
@@ -53,7 +80,7 @@ bool is_prime_miller_rabin(US_TP num, size_t rounds)
 		bool second_condition = false;
 		for (auto i = 0; i < s; i++)
 		{
-			unsigned long long int two_pow_s = pow(2, i);
+			unsigned long long int two_pow_s = (unsigned long long int)pow(2, i);
 			US_TP two_pow_s_mul_d = (US_TP)two_pow_s * d;
 			US_TP x = power_mod(rand_num, two_pow_s_mul_d, num);
 #ifdef DEBUG_PRINT
@@ -74,6 +101,8 @@ bool is_prime_miller_rabin(US_TP num, size_t rounds)
 	return true;
 }
 
+#define OPTIMIZE_RABIN
+#ifdef OPTIMIZE_RABIN
 bool is_prime_miller_rabin_optimize(US_TP num, size_t rounds)
 {
 	if (num <= 3) return true;
@@ -89,12 +118,12 @@ bool is_prime_miller_rabin_optimize(US_TP num, size_t rounds)
 	}
 	if (d == 0) // в случае разложения числа на степень двойки будет 2^s * d(1)
 		d = 1;
-	srand(time(NULL));
+	rd::mt19937 mt;
+	mt.seed(time(NULL));
+	rd::uniform_int_distribution<US_TP> ui(2, num - 2);
 	for (auto i = 0; i < rounds; i++)
 	{
-		US_TP rand_num = 0;
-		while (rand_num < 2 || rand_num > num - 2)
-			rand_num = rand() % (num);
+		US_TP rand_num = ui(mt);
 		US_TP a_pow_d_mod_n = power_mod(rand_num, d, num);
 		if (a_pow_d_mod_n == 1)
 			continue;
@@ -119,6 +148,7 @@ bool is_prime_miller_rabin_optimize(US_TP num, size_t rounds)
 	}
 	return true; //Может возвращать вероятность?
 }
+#endif
 
 US_TP power_mod(US_TP value, US_TP power, US_TP mod)
 {
--- PrimeNumbs/PrimeNumbsKPEq.h
+++ PrimeNumbs/PrimeNumbsKPEq.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <boost/multiprecision/cpp_int.hpp>
+#include <boost/random.hpp>
 #include <iostream>
 #include <random>
+#include <limits>
 
 namespace mp = boost::multiprecision;
-#define US_TP mp::int1024_t
+namespace rd = boost::random;
+#define US_TP mp::cpp_int
 
 bool is_prime(unsigned long long int num);
 bool is_prime_miller_rabin(US_TP num, size_t rounds);
