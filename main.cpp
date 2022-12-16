d193717f49a2a3f28502a2db0e47980eb69d7104
kirpitcheq <kirpitcheq@ya.ru>
2022-12-16 17:45:32 +0300

    Correct manual mode prog

--- main.cpp
+++ main.cpp
@@ -9,15 +9,33 @@ int main()
 	while (numb)
 	{
 		std::cin >> numb;
-		std::cout << is_prime(numb) << std::endl;
-		std::cout << is_prime_miller_rabin(numb, 100) << std::endl;
-		std::cout << is_prime_miller_rabin_optimize(numb, 100) << std::endl << std::endl;
+		bool brute_force = is_prime(numb);
+		bool miller_rab = is_prime_miller_rabin(numb);
+		bool miller_rab_optim = is_prime_miller_rabin_optimize(numb);
+
+		std::cout << "Brute force method: ";
+		if (brute_force)
+			std::cout << "it's Prime!" << std::endl;
+		else
+			std::cout << "it's Not prime!" << std::endl;
+
+		std::cout << "Miller-Rabin method: ";
+		if (miller_rab)
+			std::cout << "it's Prime!" << std::endl;
+		else
+			std::cout << "it's Not prime!" << std::endl;
+		std::cout << "Optimized Miller-Rabin method: ";
+
+		if (miller_rab_optim)
+			std::cout << "it's Prime!" << std::endl;
+		else
+			std::cout << "it's Not prime!" << std::endl;
 	}
 }
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
+// Советы по началу работы
 //   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
 //   2. В окне Team Explorer можно подключиться к системе управления версиями.
 //   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
